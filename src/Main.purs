7fa03fb72f051fa353ad9358814f10d9c76fd145
Chris Penner <christopher.penner@gmail.com>
2017-10-09 23:26:18 -0600

    User ReaderT for interface

--- src/Main.purs
+++ src/Main.purs
@@ -1,23 +1,27 @@
 module Main where
 
-import Control.Monad.Aff (liftEff', runAff_)
+import Control.Monad.Aff (runAff_)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Node.ReadLine (createConsoleInterface, noCompletion, READLINE)
+import Control.Monad.Reader (runReaderT)
+import Data.Maybe (Maybe(..))
+import Data.String (uncons)
+import Node.ReadLine (READLINE, createConsoleInterface, noCompletion)
 import Node.ReadLine.Aff (question, readLine)
 import Prelude (Unit, bind, const, discard, pure, unit, ($), (<<<), (<>))
 
 main :: forall e. Eff (console :: CONSOLE, readline :: READLINE, exception :: EXCEPTION | e) Unit
 main = do
   interface <- createConsoleInterface noCompletion 
-  runAff_ (const $ pure unit) (loop interface)
+  runAff_ (const $ pure unit) (runReaderT loop interface)
   where
-    loop interface = do
-      dog <- question interface "What's your dog?\n"
-      liftEff' <<< log $ "Can I pet " <> dog <> "?"
-      str <- readLine interface
-      liftEff' $ log str
-      s2 <- readLine interface
-      liftEff' <<< log $ "second:" <> s2
-      loop interface
+    loop = do
+      dog <- question "What's your dog's name?\n"
+      liftEff <<< log $ "Can I pet " <> dog <> "?"
+      str <- readLine
+      case uncons str of
+        Just {head: 'y'} -> liftEff $ log "Thanks!"
+        _ -> liftEff $ log "C'mon! Be a sport about it!"
+      loop
--- src/Node/ReadLine/Aff.purs
+++ src/Node/ReadLine/Aff.purs
@@ -7,50 +7,62 @@ module Node.ReadLine.Aff
   , module RLExports
   ) where
 
-import Prelude (Unit, discard, pure, (<<<), (*>))
-import Data.Either(Either(..))
-import Node.ReadLine 
-  ( output, completer, terminal, historySize, noCompletion,  createInterface,createConsoleInterface
-  , Completer, Interface
-  ) as RLExports
-import Node.ReadLine as RL
 import Control.Monad.Aff
+
+import Control.Monad.Aff.Class (class MonadAff, liftAff)
+import Control.Monad.Eff.Class (class MonadEff, liftEff)
+import Control.Monad.Reader (class MonadAsk, ask)
+import Data.Either (Either(..))
+import Node.ReadLine (output, completer, terminal, historySize, noCompletion, createInterface, createConsoleInterface, Completer, Interface) as RLExports
+import Node.ReadLine as RL
+import Prelude (class Monad, Unit, bind, discard, pure, ($), (*>), (<<<), (>>=), (>>>))
 
 -- | Writes a query to the output, waits
 -- | for user input to be provided on input, then invokes
 -- | the callback function
 question
-  :: forall eff
-   . RL.Interface
-  -> String
-  -> Aff (readline :: RL.READLINE | eff) String
-question interface q = makeAff go
+  :: forall eff m
+   . Monad m
+  => MonadAff (readline :: RL.READLINE | eff) m
+  => MonadAsk RL.Interface m
+  => String
+  -> m String
+question q = do
+  interface <- ask
+  liftAff $ makeAff (go interface)
   where
-    go handler = RL.question q (handler <<< Right) interface *> pure nonCanceler
+    go interface handler = RL.question q (handler <<< Right) interface *> pure nonCanceler
 
 -- | Set the prompt.
 setPrompt
-  :: forall eff
-   . String
+  :: forall eff m
+  . MonadEff (readline :: RL.READLINE | eff) m
+  => MonadAsk RL.Interface m
+  => String
   -> Int
-  -> RL.Interface
-  -> Aff (readline :: RL.READLINE | eff) Unit
-setPrompt prompt len interface = liftEff' (RL.setPrompt prompt len interface)
+  -> m Unit
+setPrompt prompt len = do
+  interface <- ask 
+  liftEff $ RL.setPrompt prompt len interface
 
 -- | Close the specified `RL.Interface`.
 close
-  :: forall eff
-   . RL.Interface
-  -> Aff (readline :: RL.READLINE | eff) Unit
-close = liftEff' <<< RL.close
+  :: forall eff m
+  . MonadEff (readline :: RL.READLINE | eff) m 
+  => MonadAsk RL.Interface m
+  => m Unit
+close = ask >>= (RL.close >>> liftEff)
 
 readLine 
-  :: forall eff
-  . RL.Interface
-  -> Aff (readline :: RL.READLINE | eff) String
-readLine interface = makeAff go
+  :: forall eff m
+  . MonadAff (readline :: RL.READLINE | eff) m 
+  => MonadAsk RL.Interface m
+  => m String
+readLine = do
+  interface <- ask
+  liftAff $ makeAff (go interface)
   where
-    go handler = do
+    go interface handler = do
       RL.setLineHandler interface (handler <<< Right) 
       RL.prompt interface
       pure nonCanceler
